fd6ccb3668b52ed4aecbec8d5787aa8cdf33a441
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-03-18 16:02:21 +0500

    Added TypeWriter JS

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+// Swiper Slider
 var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
@@ -7,3 +8,19 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
 });
+
+// TypeWriter JS Typing Effect For Desc
+var introTypeWriterEffect = document.getElementById('introTypeWriterEffect');
+var typewriterIntro = new Typewriter(introTypeWriterEffect, {
+    loop: true,
+    cursor: ""
+});
+typewriterIntro.typeString('<h1>Hi, I am <span>Imran Haider</span> </h1>')
+    .pauseFor(100)
+    .deleteChars()
+    .typeString('A Guy Pasionate in JavaScript')
+    .pauseFor(10)
+    .deleteChars(4)
+    .typeString('Hello World')
+    .pauseFor(10)
+    .start();
