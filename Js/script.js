ae6247bc90fc296f33f516c165de3651f94d3239
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-03-19 13:36:02 +0500

    Added TypeWriter Js and Animation on Image

--- Js/script.js
+++ Js/script.js
@@ -10,17 +10,44 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 // TypeWriter JS Typing Effect For Desc
-var introTypeWriterEffect = document.getElementById('introTypeWriterEffect');
-var typewriterIntro = new Typewriter(introTypeWriterEffect, {
+var descTypeWriterEffect = document.getElementById('descTypeWriterEffect');
+var typewriterDesc = new Typewriter(descTypeWriterEffect, {
     loop: true,
-    cursor: ""
 });
-typewriterIntro.typeString('<h1>Hi, I am <span>Imran Haider</span> </h1>')
-    .pauseFor(100)
-    .deleteChars()
-    .typeString('A Guy Pasionate in JavaScript')
-    .pauseFor(10)
-    .deleteChars(4)
-    .typeString('Hello World')
-    .pauseFor(10)
+typewriterDesc.typeString('I am a Web Developer.')
+    .pauseFor(2000)
+    .deleteChars(16)
+    .typeString('Passionate in FrontEnd Dev.')
+    .pauseFor(2000)
     .start();
+document.getElementById('descTypeWriterEffect').style.backgroundColor = 'black';
+document.getElementById('descTypeWriterEffect').style.color = 'white';
+document.getElementById('descTypeWriterEffect').style.paddingLeft = '5px';
+
+// Third Line
+var descTypeWriterEffect1 = document.getElementById('descTypeWriterEffect1');
+var typewriterDesc1 = new Typewriter(descTypeWriterEffect1, {
+    loop: true,
+});
+typewriterDesc1.typeString('Lets Work Together.')
+    .pauseFor(2000)
+    .deleteChars()
+    .start();
+document.getElementById('descTypeWriterEffect1').style.marginTop = '5px';
+document.getElementById('descTypeWriterEffect1').style.paddingLeft = '5px';
+document.getElementById('descTypeWriterEffect1').style.backgroundColor = 'black';
+document.getElementById('descTypeWriterEffect1').style.color = 'white';
+
+// Fourth Line
+var descTypeWriterEffect2 = document.getElementById('descTypeWriterEffect2');
+var typewriterDesc2 = new Typewriter(descTypeWriterEffect2, {
+    loop: true,
+});
+typewriterDesc2.typeString('For Further Info Contact Me!.')
+    .pauseFor(2000)
+    .deleteChars()
+    .start();
+document.getElementById('descTypeWriterEffect2').style.marginTop = '5px';
+document.getElementById('descTypeWriterEffect2').style.paddingLeft = '5px';
+document.getElementById('descTypeWriterEffect2').style.backgroundColor = 'black';
+document.getElementById('descTypeWriterEffect2').style.color = 'white';
